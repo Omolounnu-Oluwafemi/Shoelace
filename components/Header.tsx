cc929c818da6bb1be119c987c9a44f6bf71366b7
Omolounnu-Oluwafemi <oluwafemiomolounnu@gmail.com>
2026-01-20 15:34:00 +0100

    updated header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,7 +36,7 @@ export default function Header() {
                 className="capitalize font-semibold text-sm text-gray-700 hover:text-yellow-600 transition relative group"
               >
                 {item.label}
-                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-gradient-to-r from-yellow-500 to-yellow-600 group-hover:w-full transition-all duration-300"></span>
+                <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-linear-to-r from-yellow-500 to-yellow-600 group-hover:w-full transition-all duration-300"></span>
               </Link>
             ))}
           </nav>
